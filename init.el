94738505117c7726cde433cd3dcc51e737bbd17f
Bozhidar Batsov <bozhidar@batsov.com>
2016-01-31 00:45:42 +0200

    Invoke whitespace-cleanup on save

--- init.el
+++ init.el
@@ -263,6 +263,7 @@
   :init
   (dolist (hook '(prog-mode-hook text-mode-hook))
     (add-hook hook #'whitespace-mode))
+  (add-hook 'before-save-hook #'whitespace-cleanup)
   :config
   (setq whitespace-line-column 80) ;; limit line length
   (setq whitespace-style '(face tabs empty trailing lines-tail)))
