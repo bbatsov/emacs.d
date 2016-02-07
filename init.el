a0083e9c4a839b7f3ec2775030183446d8462ab5
Bozhidar Batsov <bozhidar@batsov.com>
2016-02-07 12:11:17 +0200

    Add aggressive-indent-mode

--- init.el
+++ init.el
@@ -427,6 +427,11 @@
          ([remap kill-whole-line] . crux-kill-whole-line)
          ("C-c i" . crux-ispell-word-then-abbrev)))
 
+(use-package aggressive-indent
+  :ensure t
+  :config
+  (global-aggressive-indent-mode +1))
+
 (use-package undo-tree
   :ensure t
   :config
