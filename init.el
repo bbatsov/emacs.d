df64052d66b66bbd3da234590a5dd5aa72319806
Bozhidar Batsov <bozhidar@batsov.com>
2016-02-25 23:45:02 +0200

    Fix hook name

--- init.el
+++ init.el
@@ -412,7 +412,7 @@ Start `ielm' if it's not already running."
   (setq ispell-program-name "aspell" ; use aspell instead of ispell
         ispell-extra-args '("--sug-mode=ultra"))
   (add-hook 'text-mode-hook #'flyspell-mode)
-  (add-hook 'prog-mode #'flyspell-prog-mode))
+  (add-hook 'prog-mode-hook #'flyspell-prog-mode))
 
 (use-package flycheck
   :ensure t
