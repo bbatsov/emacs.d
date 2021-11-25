b96c250cb53b8c4f80c82d4c06f716abe240c61f
Bozhidar Batsov <bozhidar@batsov.dev>
2021-11-25 14:13:24 +0200

    Remove the legacy ivy config
    It's safe to say that selectrum and consul are a good replacement
    for my use-cases.

--- init.el
+++ init.el
@@ -648,36 +648,6 @@ Start `ielm' if it's not already running."
   (global-undo-tree-mode +1)
   (diminish 'undo-tree-mode))
 
-;; (use-package ivy
-;;   :ensure t
-;;   :config
-;;   (ivy-mode 1)
-;;   (setq ivy-use-virtual-buffers t)
-;;   (setq enable-recursive-minibuffers t)
-;;   (global-set-key (kbd "C-c C-r") 'ivy-resume)
-;;   (global-set-key (kbd "<f6>") 'ivy-resume))
-;;
-;; (use-package swiper
-;;   :ensure t
-;;   :config
-;;   (global-set-key "\C-s" 'swiper))
-;;
-;; (use-package counsel
-;;   :ensure t
-;;   :config
-;;   (global-set-key (kbd "M-x") 'counsel-M-x)
-;;   (global-set-key (kbd "C-x C-f") 'counsel-find-file)
-;;   (global-set-key (kbd "<f1> f") 'counsel-describe-function)
-;;   (global-set-key (kbd "<f1> v") 'counsel-describe-variable)
-;;   (global-set-key (kbd "<f1> l") 'counsel-find-library)
-;;   (global-set-key (kbd "<f2> i") 'counsel-info-lookup-symbol)
-;;   (global-set-key (kbd "<f2> u") 'counsel-unicode-char)
-;;   (global-set-key (kbd "C-c g") 'counsel-git)
-;;   (global-set-key (kbd "C-c j") 'counsel-git-grep)
-;;   (global-set-key (kbd "C-c a") 'counsel-ag)
-;;   (global-set-key (kbd "C-x l") 'counsel-locate)
-;;   (define-key minibuffer-local-map (kbd "C-r") 'counsel-minibuffer-history))
-
 (use-package ace-window
   :ensure t
   :config
