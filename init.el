0f8ece01cb61fc521ec49309a885d9b5d41c7dd0
Bozhidar Batsov <bozhidar@batsov.dev>
2021-11-25 14:12:48 +0200

    Add the consult package

--- init.el
+++ init.el
@@ -503,6 +503,43 @@ Start `ielm' if it's not already running."
   (selectrum-prescient-mode +1)
   (prescient-persist-mode +1))
 
+(use-package consult
+  :ensure t
+  :bind (
+         ;; C-x bindings (ctl-x-map)
+         ("C-x M-:" . consult-complex-command)     ;; orig. repeat-complex-command
+         ("C-x b" . consult-buffer)                ;; orig. switch-to-buffer
+         ("C-x 4 b" . consult-buffer-other-window) ;; orig. switch-to-buffer-other-window
+         ("C-x 5 b" . consult-buffer-other-frame)  ;; orig. switch-to-buffer-other-frame
+         ;; Custom M-# bindings for fast register access
+         ("M-#" . consult-register-load)
+         ("M-'" . consult-register-store)          ;; orig. abbrev-prefix-mark (unrelated)
+         ("C-M-#" . consult-register)
+         ;; Other custom bindings
+         ("M-y" . consult-yank-pop)                ;; orig. yank-pop
+         ("<help> a" . consult-apropos)            ;; orig. apropos-command
+         ;; M-g bindings (goto-map)
+         ("M-g e" . consult-compile-error)
+         ("M-g f" . consult-flycheck)
+         ("M-g g" . consult-goto-line)             ;; orig. goto-line
+         ("M-g M-g" . consult-goto-line)           ;; orig. goto-line
+         ("M-g o" . consult-outline)               ;; Alternative: consult-org-heading
+         ("M-g m" . consult-mark)
+         ("M-g k" . consult-global-mark)
+         ("M-g i" . consult-imenu)
+         ("M-g I" . consult-imenu-multi)
+         ;; M-s bindings (search-map)
+         ("M-s f" . consult-find)
+         ("M-s F" . consult-locate)
+         ("M-s g" . consult-grep)
+         ("M-s G" . consult-git-grep)
+         ("M-s r" . consult-ripgrep)
+         ("M-s l" . consult-line)
+         ("M-s L" . consult-line-multi)
+         ("M-s m" . consult-multi-occur)
+         ("M-s k" . consult-keep-lines)
+         ("M-s u" . consult-focus-lines)))
+
 (use-package company
   :ensure t
   :config
@@ -528,6 +565,7 @@ Start `ielm' if it's not already running."
   :bind (("M-z" . zop-up-to-char)
          ("M-Z" . zop-to-char)))
 
+;; TODO: can be removed in favor of consult
 (use-package imenu-anywhere
   :ensure t
   :bind (("C-c i" . imenu-anywhere)
