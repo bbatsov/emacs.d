7a42eabfab91f028a26f02f592a5028c8ca031f3
Bozhidar Batsov <bozhidar@batsov.dev>
2025-02-06 19:57:06 +0200

    Remove company leftovers

--- init.el
+++ init.el
@@ -724,20 +724,6 @@ Start `ielm' if it's not already running."
   :custom
   (dabbrev-ignored-buffer-regexps '("\\.\\(?:pdf\\|jpe?g\\|png\\)\\'")))
 
-;; (use-package company
-;;   :ensure t
-;;   :config
-;;   (setq company-idle-delay 0.5)
-;;   (setq company-show-numbers t)
-;;   (setq company-tooltip-limit 10)
-;;   (setq company-minimum-prefix-length 2)
-;;   (setq company-tooltip-align-annotations t)
-;;   ;; invert the navigation direction if the the completion popup-isearch-match
-;;   ;; is displayed on top (happens near the bottom of windows)
-;;   (setq company-tooltip-flip-when-above t)
-;;   (global-company-mode)
-;;   (diminish 'company-mode))
-
 (use-package hl-todo
   :config
   (setq hl-todo-highlight-punctuation ":")
