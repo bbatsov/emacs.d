2f9f81bcd23f36ad248848ee4a69c45c0a793205
Bozhidar Batsov <bozhidar@batsov.dev>
2026-04-01 15:18:03 +0300

    Remove treesit-auto (redundant on Emacs 30+)

--- init.el
+++ init.el
@@ -345,13 +345,6 @@ are defining or executing a macro."
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;; third-party packages
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-(use-package treesit-auto
-  :custom
-  (treesit-auto-install 'prompt)
-  :config
-  (treesit-auto-add-to-auto-mode-alist 'all)
-  (global-treesit-auto-mode))
-
 ;;; color themes
 
 (use-package zenburn-theme
