6880fa90ff5d1fe396a0cd2f143e681aade11d9d
Bozhidar Batsov <bozhidar@batsov.dev>
2026-04-03 17:16:09 +0300

    Enable read-extended-command-predicate to filter irrelevant M-x commands

--- init.el
+++ init.el
@@ -675,10 +675,9 @@ are defining or executing a macro."
         '(read-only t cursor-intangible t face minibuffer-prompt))
   (add-hook 'minibuffer-setup-hook #'cursor-intangible-mode)
 
-  ;; Emacs 28: Hide commands in M-x which do not work in the current mode.
-  ;; Vertico commands are hidden in normal buffers.
-  ;; (setq read-extended-command-predicate
-  ;;       #'command-completion-default-include-p)
+  ;; hide commands in M-x which do not work in the current mode
+  (setq read-extended-command-predicate
+        #'command-completion-default-include-p)
 
   ;; Enable recursive minibuffers
   (setq enable-recursive-minibuffers t))
