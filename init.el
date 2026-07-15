a41e99d5b0d57738d15c136fb8033559930df85e
Bozhidar Batsov <bozhidar@batsov.dev>
2026-07-15 19:12:48 +0300

    Drop binding for the defunct consult-multi-occur
    The command was removed from consult; consult-line-multi (M-s L) is
    its replacement.

--- init.el
+++ init.el
@@ -828,7 +828,6 @@ global one."
          ("M-s r" . consult-ripgrep)
          ("M-s l" . consult-line)
          ("M-s L" . consult-line-multi)
-         ("M-s m" . consult-multi-occur)
          ("M-s k" . consult-keep-lines)
          ("M-s u" . consult-focus-lines)))
 
